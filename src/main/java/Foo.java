9001c1ff0d1102c6ea0a6e092b548af0a169fa69
sergei-okon <sergei.okon@gmail.com>
2022-02-07 18:54:54 +0200

    Refactoring task

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,18 +1,35 @@
-public class Foo extends Thread {
+import java.util.concurrent.Semaphore;
 
-    private String print(String str) {
-        return str;
+public class Foo {
+
+    public Semaphore semaphoreForSecond = new Semaphore(0);
+    public Semaphore semaphoreForThird = new Semaphore(0);
+
+    private synchronized void print(String str) {
+        System.out.print(str);
     }
 
-    public String first() {
-        return print("first");
+    public void first() {
+        print("first");
+        semaphoreForSecond.release();
     }
 
-    public String second() {
-        return print("second");
+    public void second() {
+        try {
+            semaphoreForSecond.acquire();
+            print("second");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        semaphoreForThird.release();
     }
 
-    public String third() {
-        return print("third");
+    public void third() {
+        try {
+            semaphoreForThird.acquire();
+            print("third");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/FooTest.java
+++ src/main/java/FooTest.java
@@ -1,27 +1,13 @@
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.concurrent.*;
 
 public class FooTest {
 
     public static void main(String[] args) {
         Foo foo = new Foo();
 
-        CompletableFuture<String> future3
-                = CompletableFuture.supplyAsync(foo::third);
-
-        CompletableFuture<String> future1
-                = CompletableFuture.supplyAsync(foo::first);
-
-        CompletableFuture<String> future2
-                = CompletableFuture.supplyAsync(foo::second);
-
-        String combined = Stream.of(future1, future2, future3)
-                .map(CompletableFuture::join)
-                .collect(Collectors.joining());
-
-        System.out.println(combined);
-
+        CompletableFuture.runAsync(foo::third);
+        CompletableFuture.runAsync(foo::first);
+        CompletableFuture.runAsync(foo::second);
     }
 }
 
